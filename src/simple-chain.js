8e99ca16360611e71e03186191af8db4ea5af41a
vakxaminas <sverxkabzdec2019@gmail.com>
2021-02-02 19:09:37 +0300

    feat: solve simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+  glue: "~~",
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = "") {
+    this.chain.push(String(value));
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      this.chain[position] === undefined ||
+      typeof position !== "number" ||
+      Math.round(position) !== position
+    ) {
+      this.chain = [];
+      throw new Error("Invalid position");
+    }
+    this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let arrWithBrackets = this.chain.map((item) => "( " + item + " )");
+    this.chain = [];
+    return arrWithBrackets.join(this.glue);
+  },
 };
 
 module.exports = chainMaker;
