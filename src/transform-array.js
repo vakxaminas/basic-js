07baa9456e054d66c1c4ecabe7c4f46c71c9ad9f
vakxaminas <sverxkabzdec2019@gmail.com>
2021-02-02 18:29:43 +0300

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const sequences = [
+  "--discard-next",
+  "--discard-prev",
+  "--double-next",
+  "--double-prev",
+];
+
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("Not an array");
+
+  let arrCopy = arr.slice();
+  let transformedArr = [];
+
+  for (let i = 0; i < arrCopy.length; i++) {
+    if (sequences.includes(arrCopy[i])) {
+      if (arrCopy[i] == "--discard-next" && i !== arrCopy.length - 1) {
+        transformedArr.push(arrCopy[i]);
+        arrCopy.splice(i + 1, 1);
+      }
+      if (arrCopy[i] == "--double-next" && i !== arrCopy.length - 1) {
+        transformedArr.push(arrCopy[i]);
+        arrCopy.splice(i + 1, 0, arrCopy[i + 1]);
+      }
+      if (arrCopy[i] == "--discard-prev" && i !== 0) {
+        transformedArr.pop();
+        transformedArr.push(arrCopy[i]);
+      }
+      if (arrCopy[i] == "--double-prev" && i !== 0) {
+        transformedArr.push(arrCopy[i - 1]);
+        transformedArr.push(arrCopy[i]);
+      }
+    } else transformedArr.push(arrCopy[i]);
+  }
+
+  transformedArr = transformedArr.filter((item) => !sequences.includes(item));
+
+  return transformedArr;
 };
